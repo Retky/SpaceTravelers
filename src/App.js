1fa540f113f75095966c9401e9b759ed776fad0e
Retky <jose.arredondo.a@outlook.com>
2022-05-09 14:21:54 -0500

    Add 'Rockets' component & link it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
 import Nav from './components/nav';
+import Rockets from './components/rockets';
 
 function App() {
   return (
     <Router>
       <Nav />
+      <section>
+        <Routes>
+          <Route path="/" element={<Rockets />} />
+        </Routes>
+      </section>
     </Router>
   );
 }
