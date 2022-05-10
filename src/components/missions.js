908123146a55c958e3caf3ba0996801149bfcf68
tafaramafemba <tafara.maf@gmail.com>
2022-05-10 14:02:59 +0200

    setup basic ui for missions

--- src/components/missions.js
+++ src/components/missions.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const Missions = () => (
-  <p>Hello World x2!</p>
-);
+function Missions(){
+  const state = useSelector(state => state.missions);
+  console.log(state);
 
+  return(
+    <table>
+    <thead>
+      <tr>
+      <th>Mission</th>
+      <th>Description</th>
+      <th>Status</th>
+      <th></th>
+      </tr>
+    </thead>
+    {state.map(mission => (
+      <tbody key={mission.mission_id}>
+        <td>{mission.mission_name}</td>
+        <td>{mission.description}</td>
+        <td><button>not a member</button></td>
+        <td><button>join mission</button></td>
+      </tbody>
+    )
+    )}
+
+  </table>
+  )
+}
+
+   
 export default Missions;
