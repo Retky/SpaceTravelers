985576f81643d4035643cdcf66872e513ba149d1
tafaramafemba <tafara.maf@gmail.com>
2022-05-10 19:54:54 +0200

    add styling and join missions action creator

--- src/components/missions.js
+++ src/components/missions.js
@@ -1,15 +1,20 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { showMissions } from '../redux/missions/Missions';
+import { showMissions, joinMissions } from '../redux/missions/Missions';
 import '../missions.css';
 
 function Missions() {
   const state = useSelector((state) => state.missions);
   const dispatch = useDispatch();
+  console.log(state);
 
   useEffect(() => {
     dispatch(showMissions());
   }, []);
+
+  const handleJoin = (e) => {
+    dispatch(joinMissions(e.target.id))
+  }
 
   return (
     <div>
@@ -28,7 +33,7 @@ function Missions() {
             <td>{mission.mission_name}</td>
             <td className='description-td'>{mission.description}</td>
             <td className='member'><button type="button">not a member</button></td>
-            <td className='join'><button type="button">join mission</button></td>
+            <td className='join'><button type="button" id={mission.mission_id} onClick={handleJoin}>join mission</button></td>
           </tr>
         </tbody>
       ))}
