76eeddf4e7501b67c20135507c637be0e2b58eff
tafaramafemba <tafara.maf@gmail.com>
2022-05-10 16:43:47 +0200

    fix linter error

--- src/components/missions.js
+++ src/components/missions.js
@@ -17,16 +17,15 @@ function Missions() {
           <th>Mission</th>
           <th>Description</th>
           <th>Status</th>
-          <th />
         </tr>
       </thead>
       {state.map((mission) => (
         <tbody key={mission.mission_id}>
           <tr>
-          <td>{mission.mission_name}</td>
-          <td>{mission.description}</td>
-          <td><button type='button'>not a member</button></td>
-          <td><button type='button'>join mission</button></td>
+            <td>{mission.mission_name}</td>
+            <td>{mission.description}</td>
+            <td><button type="button">not a member</button></td>
+            <td><button type="button">join mission</button></td>
           </tr>
         </tbody>
       ))}
